f0f7f3aa14dc4b1e841a99527fc98b2fdf136b26
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-06-17 11:46:48 +0530

    TabListContextMenuOptions and TabItemLayout sample committed

---
+++ Samples/TabListContextMenuOptions/ViewModel/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Syncfusion.Windows.Shared;
+using Syncfusion.Windows.Tools.Controls;
+using System.Collections.ObjectModel;
+
+namespace TabListContextMenuOptions
+{
+    public class ViewModel : NotificationObject
+    {
+
+        private TabListContextMenuOptions tabListContextMenuOptions = TabListContextMenuOptions.ShowHiddenItems;
+        private ObservableCollection<object> tabItems;
+
+        public ObservableCollection<object> TabItems
+        {
+            get
+            {
+                return tabItems;
+            }
+            set
+            {
+                tabItems = value;
+                this.RaisePropertyChanged(nameof(TabItems));
+            }
+        }
+
+        public TabListContextMenuOptions TabListContextMenuOptions
+        {
+            get { return tabListContextMenuOptions; }
+            set
+            {
+                tabListContextMenuOptions = value;
+                this.RaisePropertyChanged(nameof(TabListContextMenuOptions));
+            }
+        }
+
+        public ViewModel()
+        {
+            TabItems = new ObservableCollection<object>();
+        }
+    }
+}
